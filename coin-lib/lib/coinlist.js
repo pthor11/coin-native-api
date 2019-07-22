01b4e3ed543c453b1fcab91064f6bb8b8eba2789
Phương <phuonglm@me.com>
2019-07-22 08:42:43 +0700

    fix btc address generator testnet

--- coin-lib/lib/coinlist.js
+++ coin-lib/lib/coinlist.js
@@ -1,10 +1,23 @@
 import bitcoinjs from './bitcoinjs-3.3.2'
 
+// MAINET
+
+// export default {
+//     btc: { id: 0, network : bitcoinjs.bitcoin.networks.bitcoin },
+//     eth: { id: 60, network : bitcoinjs.bitcoin.networks.bitcoin },
+//     etc: { id: 61, network : bitcoinjs.bitcoin.networks.bitcoin },
+//     trx: { id: 195, network : bitcoinjs.bitcoin.networks.bitcoin },
+//     ltc: { id: 2, network : bitcoinjs.bitcoin.networks.litecoin }, 
+//     bch: { id: 145, network : bitcoinjs.bitcoin.networks.bitcoin },
+// }
+
+// TESTNET
+
 export default {
-    btc: { id: 0, network : bitcoinjs.bitcoin.networks.bitcoin },
-    eth: { id: 60, network : bitcoinjs.bitcoin.networks.bitcoin },
-    etc: { id: 61, network : bitcoinjs.bitcoin.networks.bitcoin },
-    trx: { id: 195, network : bitcoinjs.bitcoin.networks.bitcoin },
-    ltc: { id: 2, network : bitcoinjs.bitcoin.networks.litecoin },
-    bch: { id: 145, network : bitcoinjs.bitcoin.networks.bitcoin },
+    btc: { id: 1, network : bitcoinjs.bitcoin.networks.testnet },
+    eth: { id: 60, network : bitcoinjs.bitcoin.networks.testnet },
+    etc: { id: 61, network : bitcoinjs.bitcoin.networks.testnet },
+    trx: { id: 195, network : bitcoinjs.bitcoin.networks.testnet },
+    ltc: { id: 1, network : bitcoinjs.bitcoin.networks.testnet }, 
+    bch: { id: 145, network : bitcoinjs.bitcoin.networks.testnet },
 }
