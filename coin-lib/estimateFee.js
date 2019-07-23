fc6a534fb75287f02c2ece40b6a4f80c4651f03a
Phương <phuonglm@me.com>
2019-07-23 16:21:11 +0700

    fix estimate fee with multi inputs

--- coin-lib/estimateFee.js
+++ coin-lib/estimateFee.js
@@ -3,46 +3,46 @@ import { Buffer } from 'buffer'
 import * as  bitcoinjs from 'bitcoinjs-lib'
 import rpc from './lib/ethRPC'
 import axios from 'axios'
-const network = bitcoinjs.networks.testnet
+import coinlist from './lib/coinlist'
 
-const estimateByteSize = async ({sender, privkey, receiver, amount}) => {
-    try {        
+const estimateByteSize = async ({ sender, privkey, receiver, amount }) => {
+    try {
         amount = parseInt(100000000 * amount)
 
-        const keyPair = bitcoinjs.ECPair.fromWIF(privkey, network)
+        const keyPair = bitcoinjs.ECPair.fromWIF(privkey, coinlist.btc.network)
         // console.log({keyPair})
-        
+
 
         const utxos_response = await axios.get(`https://chain.so/api/v2/get_tx_unspent/btctest/${sender}`)
         const utxos = utxos_response.data.data.txs
-        const balance = parseInt(100000000 * utxos.reduce((balance, utxo) => balance + utxo.value, 0))
+        const balance = parseInt(100000000 * utxos.reduce((balance, utxo) => balance + parseFloat(utxo.value), 0))
 
         if (balance === 0 || utxos.length === 0) {
             return 223
-        } 
+        }
 
         const inputs = utxos.map(utxo => { return { txid: utxo.txid, vout: utxo.output_no } })
-        console.log({inputs})
-        
-        const txb = new bitcoinjs.TransactionBuilder(network)
+        // console.log({inputs})
+
+        const txb = new bitcoinjs.TransactionBuilder(coinlist.btc.network)
         txb.setVersion(1)
-        inputs.forEach(input => {
-            txb.addInput(input.txid, input.vout)
-        })
         txb.addOutput(receiver, amount)
-        txb.addOutput(sender, balance - amount - 223)
-        txb.sign(0, keyPair)
-        
-        const estimate_raw_tx =  txb.build().toHex()
-        console.log({estimate_raw_tx})
-        
+        txb.addOutput(sender, balance - amount - feerate * bytesize)
+        for (let i = 0; i < inputs.length; i++) {
+            txb.addInput(inputs[i].txid, inputs[i].vout)
+            txb.sign(i, keyPair)
+        }
+
+        const estimate_raw_tx = txb.build().toHex()
+        console.log({ estimate_raw_tx })
+
         return Buffer.byteLength(estimate_raw_tx, 'hex')
     } catch (err) {
         throw new Error(err)
     }
 }
 
-const estimateGasLimit = async ({sender, receiver, amount, data}) => {
+const estimateGasLimit = async ({ sender, receiver, amount, data }) => {
     try {
         let payload = {
             from: sender,
@@ -51,21 +51,21 @@ const estimateGasLimit = async ({sender, receiver, amount, data}) => {
         }
         if (data) {
             payload.data = data
-        }   
+        }
         const limit = await rpc('eth_estimateGas', [payload, 'latest'])
         return parseInt(limit.data.result)
     } catch (error) {
-        console.log(error);   
+        console.log(error);
         throw new Error(error)
     }
 }
 
-export default async ({coin, sender, privkey, receiver, amount, data}) => {
+export default async ({ coin, sender, privkey, receiver, amount, data }) => {
     switch (coin) {
         case 'btc':
-            return estimateByteSize({sender, privkey, receiver, amount})
+            return estimateByteSize({ sender, privkey, receiver, amount })
         case 'eth':
-            return estimateGasLimit({sender, receiver, amount, data})
+            return estimateGasLimit({ sender, receiver, amount, data })
         default:
             break;
     }
