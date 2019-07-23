48137824080b0f9a9961dd4a8b75c181fce450f1
Phương <phuonglm@me.com>
2019-07-23 18:04:07 +0700

    fix nonce getting from pending txs instead of latest block

--- coin-lib/sendTransaction.js
+++ coin-lib/sendTransaction.js
@@ -2,13 +2,13 @@ import '../shim'
 import { Buffer } from 'buffer'
 import * as  bitcoinjs from 'bitcoinjs-lib'
 import ethUtil from './lib/ethereumjs-util'
-import {Transaction} from 'ethereumjs-tx'
+import { Transaction } from 'ethereumjs-tx'
 import btcRPC from './lib/btcRPC'
 import ethRPC from './lib/ethRPC'
 import axios from 'axios'
 import coinlist from './lib/coinlist'
 
-const sendBTC = async ({privkey, receiver, amount, fee}) => {
+const sendBTC = async ({ privkey, receiver, amount, fee }) => {
     amount = parseInt(100000000 * amount)
 
     const keyPair = bitcoinjs.ECPair.fromWIF(privkey, coinlist.btc.network)
@@ -29,7 +29,7 @@ const sendBTC = async ({privkey, receiver, amount, fee}) => {
 
         const inputs = utxos.map(utxo => { return { txid: utxo.txid, vout: utxo.output_no } })
         //console.log({inputs})
-        
+
 
         const createRawTX = (feerate = 0, bytesize = 1) => {
             const txb = new bitcoinjs.TransactionBuilder(coinlist.btc.network)
@@ -53,12 +53,12 @@ const sendBTC = async ({privkey, receiver, amount, fee}) => {
         //console.log({ feerate: fee.feerate })
         estimate_raw_tx = createRawTX(fee.feerate, 223)
         //console.log({estimate_raw_tx})
-        bytesize = Buffer.byteLength(estimate_raw_tx, 'hex') 
+        bytesize = Buffer.byteLength(estimate_raw_tx, 'hex')
         //console.log({bytesize})
 
         raw_tx = createRawTX(fee.feerate, bytesize)
         //console.log({raw_tx})
-        
+
 
         const result_response = await btcRPC('sendrawtransaction', [raw_tx])
         const result = result_response.data
@@ -76,54 +76,58 @@ const sendBTC = async ({privkey, receiver, amount, fee}) => {
 
 }
 
-const sendETH = async ({privkey, receiver, amount, fee}) => {
-    amount = amount * 1000000000000000000
-    const sender = `0x` + ethUtil.privateToAddress(`0x` + privkey).toString('hex')
-    console.log({sender})
-    
-    const transactionCount_response = await ethRPC('eth_getTransactionCount', [sender, 'latest'])
-    const nonce = transactionCount_response.data.result
-    console.log({nonce})
-    
-    if (!fee.gasprice) {
-        const gasprice_response = await ethRPC('eth_gasPrice', [])
-        fee.gasprice = gasprice_response.data.result
+const sendETH = async ({ privkey, receiver, amount, fee }) => {
+    try {
+        amount = amount * 1000000000000000000
+        const sender = `0x` + ethUtil.privateToAddress(`0x` + privkey).toString('hex')
+        console.log({ sender })
+
+        const transactionCount_response = await ethRPC('eth_getTransactionCount', [sender, 'pending'])
+        const nonce = transactionCount_response.data.result
+        console.log({ nonce })
+
+        if (!fee.gasprice) {
+            const gasprice_response = await ethRPC('eth_gasPrice', [])
+            fee.gasprice = gasprice_response.data.result
+        }
+        console.log({ gasprice: parseInt(fee.gasprice) })
+
+        const gaslimit_response = await ethRPC('eth_estimateGas', [{
+            from: sender,
+            to: receiver,
+            value: `0x` + amount.toString(16)
+        }])
+        const gaslimit = gaslimit_response.data.result
+        console.log({ gaslimit: parseInt(gaslimit) })
+
+        const tx_data = {
+            // nonce,
+            gasPrice: fee.gasprice,
+            gasLimit: gaslimit,
+            to: receiver,
+            value: '0x' + amount.toString(16)
+        }
+
+        const raw_tx = new Transaction(tx_data)
+        raw_tx.sign(Buffer.from(privkey, 'hex'))
+
+        const signedTX = raw_tx.serialize().toString('hex')
+        console.log({ signedTX })
+
+        const tx_response = await ethRPC('eth_sendRawTransaction', [`0x` + signedTX])
+        
+        return tx_response.data.result ? tx_response.data.result : tx_response.data.error.message
+    } catch (err) {
+        throw new Error(err.response.data)
     }
-    console.log({gasprice: parseInt(fee.gasprice)})
-    
-    const gaslimit_response = await ethRPC('eth_estimateGas', [{
-        from: sender,
-        to: receiver,
-        value: `0x` + amount.toString(16)
-    }])
-    const gaslimit = gaslimit_response.data.result
-    console.log({gaslimit: parseInt(gaslimit)})
-
-    const tx_data = {
-        nonce,
-        gasPrice: fee.gasprice,
-        gasLimit: gaslimit,
-        to: receiver,
-        value: '0x' + amount.toString(16)
-    }
-
-    const raw_tx = new Transaction(tx_data)
-    raw_tx.sign(Buffer.from(privkey, 'hex'))
-
-    const signedTX = raw_tx.serialize().toString('hex')
-    console.log({signedTX})
-    
-    const tx_response = await ethRPC('eth_sendRawTransaction', [`0x` + signedTX])
-    const tx = tx_response.data.result
-    return tx
 }
 
-const sendTX = ({ privkey, receiver, amount, fee = {}, coin}) => {
+const sendTX = ({ privkey, receiver, amount, fee = {}, coin }) => {
     switch (coin) {
         case 'btc':
             return sendBTC({ privkey, receiver, amount, fee })
         case 'eth':
-            return sendETH({privkey, receiver, amount, fee})
+            return sendETH({ privkey, receiver, amount, fee })
         default:
             return null
     }
